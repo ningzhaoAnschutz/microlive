86165a2f613ab3b0d076804342a4088d44463231
luisub <luisubald@gmail.com>
2026-07-13 13:06:39 -0600

    new version

--- microlive/__init__.py
+++ microlive/__init__.py
@@ -22,7 +22,7 @@ Authors:
     Nathan L. Nowling, Brian Munsky, Ning Zhao
 """
 
-__version__ = "1.0.40"
+__version__ = "1.0.41"
 __author__ = "Luis U. Aguilera, William S. Raymond, Rhiannon M. Sears, Nathan L. Nowling, Brian Munsky, Ning Zhao"
 
 # Package name (for backward compatibility)
